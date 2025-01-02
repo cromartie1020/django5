973c84a92a1131ae480df7effc029dc00c43fbc5
cromartie1020 <hrc245@mail.com>
2025-01-01 23:59:50 -0500

    Create base.heml

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
+from django.http import Http404
+from .models import Post
+def post_list(request):
+    posts = Post.published.all()
+    context={
+        'posts':posts
+    }
+    return render(request, 'blog/post_list.html', context)
 
-# Create your views here.
+def post_detail(request,id):
+    try:
+        post =Post.published.get(id=id) 
+    except Post.DoesNotExist:
+        raise Http404('No Post Found')
+        
+    context ={
+        'post':post
+    }
+    return render(request, 'blog/post_detail.html', context)
