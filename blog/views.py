388e41f240922b8ef43c8ae6536e29d11e1910b2
cromartie1020 <hrc245@mail.com>
2025-01-02 18:53:21 -0500

    Working on email

--- blog/views.py
+++ blog/views.py
@@ -1,13 +1,23 @@
-from django.core.paginator import Paginator
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.shortcuts import render, get_object_or_404
 from django.http import Http404
 from .models import Post
+from .forms import EmailPostForm
+
 def post_list(request):
     post_list = Post.published.all()
     # Pagingator with 3 posts per page
     paginator = Paginator(post_list, 3)
     page_number = request.GET.get('page',1)
-    posts=paginator.page(page_number)
+    try:
+        posts = paginator.page(page_number)
+    except PageNotAnInteger:
+        # If page_number is not an integer get the first page
+        posts = paginator.page(1)    
+    except EmptyPage:
+        # If page_number is out of range get last page of results
+        posts = paginator.page(paginator.num_pages)
+    
     context={
         'posts':posts
     }
@@ -26,3 +36,28 @@ def post_detail(request, year, month, day, post):
         'blog/post_detail.html',
         {'post': post}
     )
+def post_share(request, post_id):
+    # Retrieve post by id
+    post = get_object_or_404(
+        Post,
+        id=post_id,
+        status=Post.Status.PUBLISHED
+    )
+    if request.method == 'POST':
+        # Form was submitted
+        form = EmailPostForm(request.POST)
+        if form.is_valid():
+            # Form fields passed validation
+            cd = form.cleaned_data
+            # ... send email
+    else:
+        form = EmailPostForm()
+    return render(
+        request,
+        'blog/share.html',
+        {
+            'post': post,
+            'form': form
+        }
+    )    
+    
